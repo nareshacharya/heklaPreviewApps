8e58491f209d439695e5ffa428ae17861cf8c719
Naresh Acharya <pentapati.naresh@gmail.com>
2025-08-07 19:34:08 +0530

    Publishing generated code from Hekla

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StudentForm from '../components/StudentForm';
+import StudentList from '../components/StudentList';
 
 const Home: React.FC = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (name: string, age: number) => {
+    const newStudent = { name, age };
+    setStudents([...students, newStudent]);
+  };
+
   return (
     <div>
-      <h1>Student Details</h1>
       <StudentForm />
+      <StudentList students={students} />
     </div>
   );
 };
 
 export default Home;
-```
-
-```ts
